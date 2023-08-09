92191928175d860f469ae97bba2289c2004e3dba
Isaac Sandrick <sandrickdiniz@gmail.com>
2023-08-09 00:56:50 +0000

    Matrizes

--- src/main/java/com/example/Arrays.java
+++ src/main/java/com/example/Arrays.java
@@ -11,17 +11,17 @@ public class Arrays {
        }
        System.out.println(java.util.Arrays.toString(vetor3));
 
-       double matriz[][] = new double[2][3];
+       double matriz[][] = new double[3][4];
        for (int i = 0; i < matriz.length; i++) {
           for (int j = 0; j < matriz[i].length; j++) {
-            matriz[i][j] = i * matriz[i].length +j;
+            matriz[i][j] = (i * matriz[i].length + j+1) * 10;
             
           }
        }
 
        for (int i = 0; i < matriz.length; i++) {
         for (int j = 0; j < matriz[i].length; j++) {
-          System.out.print(matriz[i][j] + " ");
+          System.out.printf("%5.1f ", matriz[i][j]);
         }
         System.err.println();
 
