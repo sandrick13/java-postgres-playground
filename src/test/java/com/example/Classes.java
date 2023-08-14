14bdbbdd99f3a6ef6e47dfd21df41efbf4ed29d6
Isaac Sandrick <sandrickdiniz@gmail.com>
2023-08-14 01:19:28 +0000

    Classes

--- src/test/java/com/example/Classes.java
+++ src/test/java/com/example/Classes.java
@@ -1,9 +1,35 @@
 package com.example;
 
 class Cliente {
-    double renda;
-    char sexo;
-    int anoNascimento;
+    private double renda;
+    private char sexo;
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    private int anoNascimento;
+
+    public double getRenda(){
+        return renda;
+    }
+
+    public void setRenda (double renda){
+        if(renda >= 0)
+            this.renda = renda;
+        else System.out.println("A renda deve ser maior que zero");
+    }
 }
 
 public class Classes {
@@ -11,21 +37,22 @@ public class Classes {
     public static void main(String[] args) {
         System.out.println("Criando classes");
         Cliente cliente = new Cliente();
-        cliente.renda = 1000;
-        cliente.sexo = 'M';
-        cliente.anoNascimento = 1980;
-        System.out.println("Renda: " + cliente.renda);
-        System.out.println("Sexo: " + cliente.sexo);
-        System.out.println("Ano de nascimento: " + cliente.anoNascimento);
+        cliente.setRenda(renda:-1000);
+        cliente.setSexo(sexo:'M');
+        cliente.SetAnoNascimento(anoNascimento: 1980);
+        System.out.println("Renda: " + cliente.getRenda());
+        System.out.println("Sexo: " + cliente.getSexo());
+        System.out.println("Ano de nascimento: " + cliente.getAnoNascimento());
 
         Cliente cliente2 = new Cliente();
-        cliente2.renda = 2000;
-        cliente2.sexo = 'F';
-        cliente2.anoNascimento = 1970;
+        cliente2.setRenda(renda: 2000);
+        cliente2.setSexo(sexo:'s');
+        cliente2.SetAnoNascimento(anoNascimento: 1970);
+        System.out.println();
 
-        System.out.println("Renda: " + cliente2.renda);
-        System.out.println("Sexo: " + cliente2.sexo);
-        System.out.println("Ano de nascimento: " + cliente2.anoNascimento);
+        System.out.println("Renda: " + cliente2.getRenda());
+        System.out.println("Sexo: " + cliente2.getSexo());
+        System.out.println("Ano de nascimento: " + cliente2.getAnoNascimento());
 
     }
     
